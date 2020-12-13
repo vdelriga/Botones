923c14c91e0d14fea511b9e3959854be66462b18
vdelriga <vmdelrio@gmail.com>
2020-12-13 22:06:33 +0100

    reinicia progress

--- Sources/Botones/DownloadButton.swift
+++ Sources/Botones/DownloadButton.swift
@@ -30,7 +30,7 @@ public class DownloadButton: UIButton {
         progressLayer.strokeEnd = self.progress
         if( progress >= 1){
             downloaded = true
-            progress=0
+            self.set_progress(valor: 0)
             self.setImage(UIImage(named:"Anima_descargas14.png",in:Bundle.module,compatibleWith: nil),for:.normal)
         }
     }
